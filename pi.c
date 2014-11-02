44ed2f2f4f1a8617cea21fe9d0380209f73d82cd
mayone <mayone321@gmail.com>
2014-11-03 00:43:28 +0800

    1.3 version

--- pi.c
+++ pi.c
@@ -3,12 +3,12 @@
  * * Filename: 		pi.c
  * * Description: 	use ptherad to acclerate Monte Carlo method calculationg pi
  * * 
- * * Version: 		1.0
- * * Created:		2014/10/26
- * * Revision:		none
+ * * Version: 		1.3
+ * * Created:		2014/10/27
+ * * Revision:		2014/10/28
  * * Compiler: 		gcc -lpthread pi.c
  * *
- * * Author: 		Mayone
+ * * Author: 		Wayne
  * * Organization:	CoDesign 
 
  ******************************************************************************/
@@ -16,66 +16,88 @@
 #include <stdio.h>
 #include <stdlib.h>		// rand()
 #include <time.h>		// time()
+#include <unistd.h>		// sysconf()
 #include <pthread.h>
 
-#define NUM_THREADS 8
 
-
-double rand_num_gen()
-{
-	return 2 * (random()/(double)RAND_MAX) - 1;	// RAND_MAX = 2147483647
-}
+typedef struct thread_data { 
+	double *in_circle;
+	pthread_mutex_t *in_circle_lock;
+	int threadID;
+	long long num_tosses;
+}thread_data;
 
 void *toss_thread(void *tosses_of_thread_void);
 
 int main(int argc, char const *argv[])
 {
 	int i;
-	long long number_in_circle = 0;
-	long long number_of_tosses = 100000000;
+	double in_circle = 0;
+	long long number_of_tosses;
 	if(argc == 2) {
 		number_of_tosses = atoi(argv[1]);
 	}
+	else {
+		number_of_tosses = 100000000;
+	}
+
+	int NUM_THREADS = sysconf(_SC_NPROCESSORS_ONLN);
+
 	long long tosses_of_thread = number_of_tosses / NUM_THREADS;
 	long long tosses_of_remain = number_of_tosses % NUM_THREADS;
-	srandom((unsigned)time(NULL));
 
-	pthread_t threads[NUM_THREADS];
-
-	pthread_create(&threads[i], NULL, toss_thread, (void*)(tosses_of_thread + tosses_of_remain));
-	for(i = 1; i < NUM_THREADS; i++) {
-		pthread_create(&threads[i], NULL, toss_thread, (void*)tosses_of_thread);	
-	}
-
-	void *partial_number_in_circle;
-	for(i = 0; i < NUM_THREADS; i++) {
-		pthread_join(threads[i], &partial_number_in_circle);
-		number_in_circle += *(long long*)partial_number_in_circle;
-	}
+	pthread_t* threads;
+	thread_data* threadD;
+	pthread_mutex_t in_circle_lock;
 	
-	// output result
-	double pi_estimate = 4*number_in_circle/(double)number_of_tosses;
+	threads = (pthread_t*)malloc(sizeof(pthread_t) * NUM_THREADS);
+	threadD = (thread_data*)malloc(sizeof(thread_data) * NUM_THREADS);
+
+	pthread_mutex_init(&in_circle_lock, NULL);
+
+	for(i = 0; i < NUM_THREADS; i++) {
+		threadD[i].in_circle= &in_circle;
+		threadD[i].in_circle_lock = &in_circle_lock;
+		threadD[i].threadID = i;
+		if(i == 0) threadD[i].num_tosses = tosses_of_thread + tosses_of_remain;
+		else threadD[i].num_tosses = tosses_of_thread;
+		pthread_create(&threads[i], NULL, toss_thread, (void*)&threadD[i]);	
+	}
+
+	for(i = 0; i < NUM_THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	double pi_estimate = 4*in_circle/(double)number_of_tosses;
 	printf("pi = %.16lf\n", pi_estimate);
 
 	return 0;
 }
 
-void *toss_thread(void *tosses_of_thread_void)
+// thread function
+void *toss_thread(void *threadD)
 {
+	long long toss;
 	double x, y;
 	double distance_square;
-	long long tosses_of_thread = (long long)tosses_of_thread_void;
-	long long *number_in_circle = (long long*)malloc(sizeof(long long));
-	long long toss;
+	long long local_in_circle = 0;
+	unsigned seed = (unsigned)time(NULL);
+	thread_data* tData = (thread_data*)threadD;
 
-	for(toss = 0; toss < tosses_of_thread; toss++) {
-		x = rand_num_gen();		//printf("x = %lf\n", x);
-		y = rand_num_gen();		//printf("y = %lf\n", y);
+	//printf("%lld\n", tData->num_tosses);
+	
+	for(toss = 0; toss < tData->num_tosses; toss++) {
+		x = 2 * (rand_r(&seed)/(double)RAND_MAX) - 1;
+		y = 2 * (rand_r(&seed)/(double)RAND_MAX) - 1;
 		distance_square = x*x + y*y;
 		if(distance_square <= 1.0) {
-			(*number_in_circle)++;
+			local_in_circle++;
 		}
 	}
 
-	pthread_exit(number_in_circle);
+	pthread_mutex_lock(tData->in_circle_lock);
+	*(tData->in_circle) += local_in_circle;
+	pthread_mutex_unlock(tData->in_circle_lock);
+
+	pthread_exit(NULL);
 }
